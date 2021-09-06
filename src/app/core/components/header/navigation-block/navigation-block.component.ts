f3957023785f331576a0ff6b89805120c2cabdfa
Dzimitry Lvou <lvoudzimitry@gmail.com>
2021-09-06 16:53:34 +0300

    feat: get categories from service

--- src/app/core/components/header/navigation-block/navigation-block.component.ts
+++ src/app/core/components/header/navigation-block/navigation-block.component.ts
@@ -1,8 +1,18 @@
 import { Component } from '@angular/core';
+import { CategoryModel } from 'src/app/core/models/category.model';
+import { CategoryService } from 'src/app/core/services/category.service';
 
 @Component({
   selector: 'app-navigation-block',
   templateUrl: './navigation-block.component.html',
   styleUrls: ['./navigation-block.component.scss'],
 })
-export class NavigationBlockComponent {}
+export class NavigationBlockComponent {
+  categories: CategoryModel[] = [];
+
+  constructor(private categoryService: CategoryService) {
+    this.categoryService.categories$.subscribe((value) => {
+      this.categories = value;
+    });
+  }
+}
